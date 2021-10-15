a78f6c5a2edecac6900795007ba0903164d2f5b9
tayuto <70104589+tayuto@users.noreply.github.com>
2021-10-16 02:57:27 +0800

    Recover the formatDonutChartDataOriginal from sm1

---
+++ src/_utils/formatDonutChartDataOriginal.js
@@ -0,0 +1,101 @@
+import randomColor from "randomcolor";
+
+export function formatDonutChartDataOriginal(response) {
+  let rawData = response.data;
+  let labelDataMap = getlabelDataMap(rawData);
+  let xaxis = [];
+  let datasets = [];
+  let result = {};
+  for (let key in labelDataMap) {
+    if (key == "student") {
+      xaxis = labelDataMap[key];
+    } else {
+      datasets.push({
+        label: key,
+        data: labelDataMap[key].slice(),
+        backgroundColor: randomColor({
+          count: xaxis.length,
+          format: 'rgb',
+          seed: 1
+        }).slice()
+      });
+    }
+  }
+  let formattedData = {
+    labels: xaxis,
+    datasets: datasets,
+  };
+  
+  let colorForOtherElseStudents = randomColor({
+    seed: 2
+  });
+  let studentList = formattedData.labels.slice();
+  studentList.push("All");
+  for (let i = 0, len=studentList.length; i < len; i++) {
+    
+    result[studentList[i]] = formatDonutChartDataForOneStudent(formattedData, studentList[i], colorForOtherElseStudents);
+    
+  }
+  
+  result["All"]["github"] = []
+  let github_labels = result["All"]["labels"];
+  let github_commits = [];
+  let github_versions = [];
+  let github_datasets = result["All"]["datasets"];
+  for (let i = 0; i < github_datasets.length; i++) {
+    if (github_datasets[i]["label"] === "commit_count") {
+      github_commits = github_datasets[i]["data"];
+    }
+    if (github_datasets[i]["label"] === "version") {
+      github_versions = github_datasets[i]["data"];
+    }
+  }
+  for (let i = 0; i < github_labels.length; i++) {
+    result["All"]["github"].push({"name": github_labels[i], "commit_count": github_commits[i],
+      "version": github_versions[i]})
+  }
+
+  return result;
+}
+
+function formatDonutChartDataForOneStudentOriginal(formattedData, student, colorForOtherElseStudents) {
+    if (student === "All") {
+      
+      return formattedData;
+    }
+    let index = 0;
+    let excludedSum = 0;
+    for (let i = 0, len = formattedData.labels.length; i < len; i++) {
+      if (formattedData.labels[i] === student) {
+        index = i;
+      } else {
+        excludedSum += formattedData.datasets[0].data[i];
+      }
+    }
+
+    let result = {
+      labels: [student, "Other students"],
+      datasets: [{
+        label: formattedData.datasets[0].label,
+        data: [formattedData.datasets[0].data[index], excludedSum],
+        backgroundColor: [formattedData.datasets[0].backgroundColor[index], colorForOtherElseStudents]
+      }]
+    };
+
+    return result;
+  }
+
+
+function getlabelDataMapOriginal(rawData) {
+  let labelDataMap = {};
+  for (let i = 0, len = rawData.length; i < len; i++) {
+    for (let key in rawData[i]) {
+      if (!labelDataMap[key]) {
+        labelDataMap[key] = [rawData[i][key]];
+      } else {
+        labelDataMap[key].push(rawData[i][key]);
+      }
+    }
+  }
+  return labelDataMap;
+}
